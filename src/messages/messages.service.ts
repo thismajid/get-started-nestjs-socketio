f39c7eaab618082b15d3616229f0115bebb7941c
thismajid <mkhorshidian72@gmail.com>
2022-05-02 11:09:53 +0430

    return all messages

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
+import { Message } from './entities/message.entity';
 
 @Injectable()
 export class MessagesService {
+  messages: Message[] = [{ name: 'john', text: 'blah blah' }];
+
   create(createMessageDto: CreateMessageDto) {
     return 'This action adds a new message';
   }
 
   findAll() {
-    return `This action returns all messages`;
+    return this.messages;
   }
 }
